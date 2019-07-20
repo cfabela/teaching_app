57a5ba8c8126ff183449b8211c841578f682a2bf
Carlos Fabela <cfabela@itexico.com>
2019-07-19 21:53:07 -0500

    Adding pages to master detail page

--- TeachingApp/TeachingApp/Pages/MasterPage.xaml.cs
+++ TeachingApp/TeachingApp/Pages/MasterPage.xaml.cs
@@ -13,16 +13,33 @@ namespace TeachingApp.Pages
             InitializeComponent();
         }
 
-        private void BtnHome_OnClicked(object sender, EventArgs e)
+        private void TapHome_OnTapped(object sender, EventArgs e)
         {
             Detail = new NavigationPage(new HomePage());
             IsPresented = false;
         }
 
-        private void BtnChangePassword_OnClicked(object sender, EventArgs e)
+        private void TapChangePassword_OnTapped(object sender, EventArgs e)
         {
             Detail = new NavigationPage(new ChangePasswordPage());
             IsPresented = false;
+        }
+
+        private void TapInstructor_OnTapped(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new BecomeInstructorPage());
+            IsPresented = false;
+        }
+
+        private void TapFAQ_OnTapped(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new FAQPage());
+            IsPresented = false;
+        }
+
+        private void TapLogout_OnTapped(object sender, EventArgs e)
+        {
+
         }
     }
 }
