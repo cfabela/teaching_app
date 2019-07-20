dc2863e00a7beaa9576000f675e00ea194250476
Carlos Fabela <cfabela@itexico.com>
2019-07-19 21:16:14 -0500

    Adding first master detail page

--- TeachingApp/TeachingApp/App.xaml.cs
+++ TeachingApp/TeachingApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TeachingApp.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace TeachingApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new MasterPage();
         }
 
         protected override void OnStart()
---
+++ TeachingApp/TeachingApp/Pages/MasterPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeachingApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : MasterDetailPage
+    {
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHome_OnClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new HomePage());
+            IsPresented = false;
+        }
+
+        private void BtnChangePassword_OnClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new ChangePasswordPage());
+            IsPresented = false;
+        }
+    }
+}
