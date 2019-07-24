1d2db46b8b93708ac8040a141cc082308453510b
Carlos Fabela <cfabela@itexico.com>
2019-07-23 22:38:32 -0500

    Adding first sercice for register user inside the app

---
+++ TeachingApp/TeachingApp/Servies/AccountService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TeachingApp.Models;
+
+namespace TeachingApp.Servies
+{
+    public class AccountService : IAccountServiceInterface
+    {
+        public async Task<bool> RegisetUser(string email, string password, string confirmPassword)
+        {
+            var registerModel = new RegisterModel()
+            {
+                Email = email, Password = password, ConfirmPassword = confirmPassword
+            };
+
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(registerModel);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("https://teachify.azurewebsites.net/api/Account/Register", content);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ TeachingApp/TeachingApp/Servies/IAccountServiceInterfaceM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TeachingApp.Servies
+{
+    public interface IAccountServiceInterface
+    {
+        Task<bool> RegisetUser(string email, string password, string confirmPassword);
+    }
+}
